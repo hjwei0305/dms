8d58d1d929ce223741cd0ea714e49f7e73b47af3
Vision.Mac <chao2.ma@changhong.com>
2020-10-20 18:04:55 +0800

    1.默认将导入导出以二开方式实现 2.增加币种导入导出

--- mdms-service/src/main/java/com/changhong/sei/mdms/general/service/excel/CurrencyExcelService.java
+++ mdms-service/src/main/java/com/changhong/sei/mdms/general/service/excel/CurrencyExcelService.java
@@ -3,11 +3,11 @@ package com.changhong.sei.mdms.general.service.excel;
 import com.changhong.sei.mdms.common.excel.IBaseExcelService;
 
 /**
- * 实现功能：国家导入导出服务
+ * 实现功能：币种导入导出服务
  *
  * @author 马超(Vision.Mac)
  * @version 1.0.00  2020-10-12 00:35
  */
-public interface CountryExcelService extends IBaseExcelService {
+public interface CurrencyExcelService extends IBaseExcelService {
 
 }
