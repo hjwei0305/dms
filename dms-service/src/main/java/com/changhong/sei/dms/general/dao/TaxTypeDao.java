d7aacdd8570eadb0e295cedca46bf1573709b4dc
YunweiY <71304289+YunweiY@users.noreply.github.com>
2021-06-22 09:25:58 +0800

    添加税分类Dao

---
+++ dms-service/src/main/java/com/changhong/sei/dms/general/dao/TaxTypeDao.java
@@ -0,0 +1,15 @@
+package com.changhong.sei.dms.general.dao;
+
+import com.changhong.sei.core.dao.BaseEntityDao;
+import com.changhong.sei.dms.general.entity.TaxType;
+import org.springframework.stereotype.Repository;
+
+/**
+ * 税分类(TaxType)数据库访问类
+ *
+ * @author 杨沄炜
+ * @since 2021/06/22 9:02
+ */
+@Repository
+public interface TaxTypeDao extends BaseEntityDao<TaxType> {
+}
