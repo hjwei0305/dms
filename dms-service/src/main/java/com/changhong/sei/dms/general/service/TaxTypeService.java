92fa81f3288456554a7c5b869995aaa269780b22
YunweiY <71304289+YunweiY@users.noreply.github.com>
2021-06-23 09:19:02 +0800

    修改税分类(TaxType)业务逻辑实现类

--- dms-service/src/main/java/com/changhong/sei/dms/general/service/TaxTypeService.java
+++ dms-service/src/main/java/com/changhong/sei/dms/general/service/TaxTypeService.java
@@ -3,12 +3,9 @@ package com.changhong.sei.dms.general.service;
 import com.changhong.sei.core.dao.BaseEntityDao;
 import com.changhong.sei.core.service.BaseEntityService;
 import com.changhong.sei.dms.general.dao.TaxTypeDao;
-import com.changhong.sei.dms.general.dto.TaxTypeDto;
 import com.changhong.sei.dms.general.entity.TaxType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 /**
  * 税分类(TaxType)业务逻辑实现类
@@ -23,12 +20,4 @@ public class TaxTypeService extends BaseEntityService<TaxType>{
 
     @Override
     protected BaseEntityDao<TaxType> getDao() { return dao; }
-
-    /**
-     * 根据税分类名称获取同名称的税分类实体集合
-     *
-     * @param name 税分类名称
-     * @return 税分类实体集合
-     */
-    public List<TaxType> findListByProperty(String name) { return findListByProperty("name", name); }
 }
