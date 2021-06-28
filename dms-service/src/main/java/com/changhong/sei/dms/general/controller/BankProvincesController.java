ce20695daba6eab920084ebab6bf4f82d1fdc8c2
冯华 <hua.feng@changhong.com>
2021-06-28 14:34:01 +0800

    修改银行省区

--- dms-service/src/main/java/com/changhong/sei/dms/general/controller/BankProvincesController.java
+++ dms-service/src/main/java/com/changhong/sei/dms/general/controller/BankProvincesController.java
@@ -7,13 +7,22 @@ import com.changhong.sei.core.dto.serach.Search;
 import com.changhong.sei.core.service.BaseEntityService;
 import com.changhong.sei.dms.general.api.BankProvincesApi;
 import com.changhong.sei.dms.general.dto.BankProvincesDto;
+import com.changhong.sei.dms.general.dto.TaxRateDto;
 import com.changhong.sei.dms.general.entity.BankProvinces;
+import com.changhong.sei.dms.general.entity.TaxRate;
 import com.changhong.sei.dms.general.service.BankProvincesService;
+import com.changhong.sei.util.ArithUtils;
 import io.swagger.annotations.Api;
+import org.modelmapper.AbstractConverter;
+import org.modelmapper.Converter;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.PropertyMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
 
 /**
  * 银行省区(BankProvinces)控制类
@@ -30,6 +39,8 @@ public class BankProvincesController extends BaseEntityController<BankProvinces,
      */
     @Autowired
     private BankProvincesService service;
+    @Autowired
+    private ModelMapper mapper;
 
     @Override
     public BaseEntityService<BankProvinces> getService() {
@@ -45,5 +56,22 @@ public class BankProvincesController extends BaseEntityController<BankProvinces,
     @Override
     public ResultData<PageResult<BankProvincesDto>> findByPage(Search search) {
         return convertToDtoPageResult(service.findByPage(search));
+    }
+
+
+    /**
+     * 定义关联Id映射
+     */
+    @Override
+    protected void customConvertToDtoMapper() {
+        // 创建自定义映射规则
+        PropertyMap<BankProvinces, BankProvincesDto> propertyMap = new PropertyMap<BankProvinces, BankProvincesDto>() {
+            @Override
+            protected void configure() {
+                map().setRegionId(source.getRegionId());
+            }
+        };
+        // 添加映射器
+        dtoModelMapper.addMappings(propertyMap);
     }
 }
