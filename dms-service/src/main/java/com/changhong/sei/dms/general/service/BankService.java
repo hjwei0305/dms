ca8437fe1afce02af35b6cf543e797727c6c6596
冯华 <hua.feng@changhong.com>
2021-12-16 13:20:23 +0800

    银行地区查询

--- dms-service/src/main/java/com/changhong/sei/dms/general/service/BankService.java
+++ dms-service/src/main/java/com/changhong/sei/dms/general/service/BankService.java
@@ -2,11 +2,17 @@ package com.changhong.sei.dms.general.service;
 
 import com.changhong.sei.core.context.ContextUtil;
 import com.changhong.sei.core.dao.BaseEntityDao;
+import com.changhong.sei.core.dto.serach.PageResult;
+import com.changhong.sei.core.dto.serach.Search;
 import com.changhong.sei.core.service.BaseEntityService;
 import com.changhong.sei.dms.general.dao.BankDao;
 import com.changhong.sei.dms.general.entity.Bank;
+import com.changhong.sei.dms.general.entity.BankProvinces;
+import org.apache.commons.collections.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Collection;
 
 
 /**
@@ -25,8 +31,24 @@ public class BankService extends BaseEntityService<Bank> {
         return dao;
     }
 
+
+    /**
+     * 基于动态组合条件对象和分页(含排序)对象查询数据集合
+     */
+    @Override
+    public PageResult<Bank> findByPage(Search searchConfig) {
+        Collection<String> quickSearchProperties = searchConfig.getQuickSearchProperties();
+        if (CollectionUtils.isNotEmpty(quickSearchProperties) && quickSearchProperties.contains("regionName")) {
+            quickSearchProperties.remove("bankAreaName");
+            quickSearchProperties.add("bankArea.name");
+        }
+        return getDao().findByPage(searchConfig);
+    }
+
+
     /**
      * 获取一个银行
+     *
      * @param code 代码
      * @return 银行
      */
